602a183b73640e5bd1fe22c903c1c530dbdd637a
nailyk-fr <nailyk_git@nailyk.fr>
2017-03-08 20:18:45 +0100

    msm8974-common: Include AEC and fix file path
     * Include AEC
     * Fix duplicate audio_effects.conf path
       (override HAL one)
    
    Change-Id: If41c003972ae886e7012a777d5159daf0a6d5e82

--- msm8974.mk
+++ msm8974.mk
@@ -32,7 +32,7 @@ PRODUCT_PACKAGES += \
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf
+    $(LOCAL_PATH)/audio/audio_effects.conf:system/etc/audio_effects.conf
 
 PRODUCT_PACKAGES += \
     libcutilz
